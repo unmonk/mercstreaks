963324b204aa3ca67b38aca7af7e6bc01135f9ca
Scott Weaver <scott@weaverscott.com>
2023-05-12 13:58:09 -0500

    Table row, dark adjusted

--- app/admin/events/page.jsx
+++ app/admin/events/page.jsx
@@ -1,10 +1,24 @@
 import EventsList from "@/components/events/EventsList";
+import CreateEventForm from "@/components/CreateEventForm";
+import { buttonVariants } from "@/components/ui/button";
+import Link from "next/link";
 
 export default function EventsPage() {
   return (
-    <div>
-      <h1>Events</h1>
-      <EventsList />
+    <div className="flex  flex-col items-center">
+      <div className="flex flex-row items-center justify-between">
+        <h1>Events</h1>
+        <Link
+          href={"/admin/events/create"}
+          className={buttonVariants({ variant: "outline" })}
+        >
+          +Create
+        </Link>
+      </div>
+      <div className="flex flex-row">
+        <EventsList />
+        <CreateEventForm />
+      </div>
     </div>
   );
 }
